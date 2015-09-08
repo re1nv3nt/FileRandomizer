438c27fe3e468eb60b6499c550ebdfbd1e358e86
re1nv3nt <apaczy@gmail.com>
2015-09-08 19:51:42 -0400

    Answer to in-class Lab 9/8/15

--- FileRandomizer/src/cis232/lab/Randomizer.java
+++ FileRandomizer/src/cis232/lab/Randomizer.java
@@ -2,7 +2,9 @@ package cis232.lab;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Randomizer {
@@ -13,16 +15,31 @@ public class Randomizer {
 		Scanner input = new Scanner(originalFile);
 		ArrayList<String> lines = new ArrayList<>();
 		
-		while(input.hasNextLine()){
+		while(input.hasNextLine())
+		{
 			lines.add(input.nextLine());
 		}
-
-		//For testing purposes, output all the lines
-		for(String line : lines){
-			System.out.println(line);
+		
+		PrintWriter outputFile = new PrintWriter("randomized_list.txt");
+		int number;
+		Random randomNumber = new Random();
+		
+		while(!lines.isEmpty()){
+			outputFile.println(lines.remove(randomNumber.nextInt(lines.size())));
 		}
 		
+		outputFile.close();
 		input.close();
+		
+		
+
+		//For testing purposes, output all the lines
+//		for(String line : lines)
+//		{
+//			System.out.println(line);
+//		}
+//		
+		
 	}
 
 }
